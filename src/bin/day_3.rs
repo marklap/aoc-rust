441ce91b0646a1c1e2e590cb371ab896f633b4fd
marklap <marklap@gmail.com>
2024-01-26 17:48:47 -0500

    calib total is correct; actual input total is wrong

--- src/bin/day_3.rs
+++ src/bin/day_3.rs
@@ -1,12 +1,13 @@
 use std::env;
 
 use std::error::Error;
+use std::fmt::{Display, Result};
 use std::io::Error as IoError;
 use std::path::Path;
 
 // const INPUT_FILE_PATH: &str = "src/bin/day_3_input.txt";
 const INPUT_FILE_PATH: &str = "src/bin/day_3_part_1_calib.txt";
-const PART_1_CALIB_ANSWER: u32 = 4361;
+const PART_1_CALIB_ANSWER: i32 = 4361;
 
 #[derive(Debug)]
 struct Number {
@@ -17,13 +18,23 @@ struct Number {
 }
 
 impl Number {
-    fn new(row: i32, col: i32, len: i32, num: i32) -> Number {
+    fn new(row: i32, end: i32, num_str: String) -> Number {
         Number {
             row,
-            col,
-            end: col + len,
-            num,
+            col: end - num_str.len() as i32,
+            end,
+            num: num_str.parse().unwrap(),
         }
+    }
+}
+
+impl Display for Number {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result {
+        write!(
+            f,
+            "Number<row: {}, col: {}, end: {}, num: {}",
+            self.row, self.col, self.end, self.num
+        )
     }
 }
 
@@ -31,20 +42,31 @@ impl Number {
 struct Symbol {
     row: i32,
     col: i32,
+    chr: String,
+}
+
+impl Display for Symbol {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result {
+        write!(
+            f,
+            "Symbol<row: {}, col: {}, chr: {}>",
+            self.row, self.col, self.chr
+        )
+    }
 }
 
 impl Symbol {
-    fn new(row: i32, col: i32) -> Symbol {
-        Symbol { row, col }
+    fn new(row: i32, col: i32, chr: String) -> Symbol {
+        Symbol { row, col, chr }
     }
 
-    fn touches(&self, num: &Number) -> bool {
+    fn is_adjacent(&self, num: &Number) -> bool {
         // number ends >= sym col - 1
         if num.end < self.col - 1 {
             return false;
         }
 
-        // number starts <= sym col + 1
+        // number starts <= sym + 1
         if num.col > self.col + 1 {
             return false;
         }
@@ -71,56 +93,56 @@ fn input_to_nums_and_syms(input: String) -> (Vec<Number>, Vec<Symbol>) {
     for (row, line) in input.lines().enumerate() {
         let mut chars = line.chars().into_iter().clone();
         let mut col = 0i32;
+        let mut num_str = String::from("");
+        let mut in_num = false;
         while let Some(chr) = chars.next() {
-            if chr == '.' {
-                continue;
-            }
             if chr.is_numeric() {
-                let mut num = String::from(chr);
-                while let Some(nchr) = chars.next() {
-                    if !nchr.is_numeric() {
-                        break;
-                    }
-                    if nchr.is_numeric() {
-                        num.push(nchr);
-                    }
+                if !in_num {
+                    in_num = true;
                 }
-                nums.push(Number::new(
-                    row as i32,
-                    col,
-                    num.len() as i32,
-                    num.parse().unwrap(),
-                ))
+                num_str.push(chr);
+            } else {
+                if in_num {
+                    nums.push(Number::new(row as i32, col, num_str));
+                    num_str = String::from("");
+                    in_num = false;
+                }
+                if chr != '.' {
+                    syms.push(Symbol::new(row as i32, col, chr.to_string()));
+                }
             }
-            syms.push(Symbol::new(row as i32, col));
             col += 1;
         }
     }
     (nums, syms)
 }
 
-fn run() -> Option<u32> {
-    // let input_file_path =
-    //     Path::new(env::var("CARGO_MANIFEST_DIR").unwrap().as_str()).join(INPUT_FILE_PATH);
+fn run() -> Option<i32> {
+    let input_file_path =
+        Path::new(env::var("CARGO_MANIFEST_DIR").unwrap().as_str()).join(INPUT_FILE_PATH);
 
-    // let input = std::fs::read_to_string(input_file_path).expect("failed to read file to string");
+    let input = std::fs::read_to_string(input_file_path).expect("failed to read file to string");
 
-    // let matrix = input_to_matrix(input);
+    let (numbers, symbols) = input_to_nums_and_syms(input);
 
-    // let num_pos: Vec<(u32, u32, u32)> = Vec::new();
-    // let sym_pos: Vec<(u32, u32)> = Vec::new();
+    let mut total = 0i32;
+    for num in numbers.iter() {
+        println!("{}", num);
+        for sym in symbols.iter() {
+            if sym.is_adjacent(num) {
+                println!("     YES!    {}", sym);
+                total += num.num;
+                break;
+            }
+            println!("  *** NOPE *** {}", sym);
+        }
+    }
 
-    // for row in matrix {
-    //     for chr in row {
-    //         if chr.is_numeric()
-    //     }
-    // }
-
-    None
+    Some(total)
 }
 
 fn main() {
-    ()
+    println!("total: {}", run().unwrap());
 }
 
 #[cfg(test)]
@@ -128,13 +150,14 @@ mod tests {
     use super::*;
 
     #[test]
+    #[ignore = "calib only"]
     fn test_part_1_calib() {
         assert_eq!(run(), Some(PART_1_CALIB_ANSWER));
     }
 
     #[test]
-    fn test_sym_touches() {
-        let n = Number::new(4, 6, 3, 123);
+    fn test_sym_is_adjacent() {
+        let n = Number::new(4, 9, "123".into());
         let coords: Vec<(i32, i32)> = vec![
             (3, 5),
             (3, 6),
@@ -153,14 +176,14 @@ mod tests {
         ];
 
         for (row, col) in coords {
-            let s = Symbol::new(row, col);
-            assert!(s.touches(&n));
+            let s = Symbol::new(row, col, "!".into());
+            assert!(s.is_adjacent(&n));
         }
     }
 
     #[test]
-    fn test_sym_not_touches() {
-        let n = Number::new(4, 6, 3, 123);
+    fn test_sym_is_not_adjacent() {
+        let n = Number::new(4, 9, "123".into());
         let coords: Vec<(i32, i32)> = vec![
             (2, 5),
             (2, 6),
@@ -179,8 +202,8 @@ mod tests {
         ];
 
         for (row, col) in coords {
-            let s = Symbol::new(row, col);
-            assert!(!s.touches(&n));
+            let s = Symbol::new(row, col, "!".into());
+            assert!(!s.is_adjacent(&n));
         }
     }
 }
